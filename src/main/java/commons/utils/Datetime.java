d56af5c1e7bbac021ac53bda4956286e9e85ba9b
hanzhe <sith0008@e.ntu.edu.sg>
2021-03-22 11:29:29 +0800

    remove year and month

--- src/main/java/commons/utils/Datetime.java
+++ src/main/java/commons/utils/Datetime.java
@@ -1,15 +1,11 @@
 package commons.utils;
 
 public class Datetime {
-    public int year;
-    public int month;
     public int day;
     public int hour;
     public int minute;
 
-    public Datetime(int year, int month, int day, int hour, int minute) {
-        this.year = year;
-        this.month = month;
+    public Datetime(int day, int hour, int minute) {
         this.day = day;
         this.hour = hour;
         this.minute = minute;
