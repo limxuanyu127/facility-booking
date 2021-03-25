13f4ec8933864e9cdec55f57b52bef7136d175c0
limxuanyu127 <limxuanyu127@gmail.com>
2021-03-25 15:42:42 +0800

    checkpoint

---
+++ src/main/java/server/translator/translator.java
@@ -0,0 +1,19 @@
+package server.translator;
+
+import commons.requests.*;
+import commons.responses.*;
+import javafx.util.Pair;
+import server.managers.BookingManager;
+
+import java.util.*;
+
+public class translator {
+    public QueryAvailabilityResponse queryAvailability(QueryAvailabilityRequest r, BookingManager bookingManager, Hashtable facilTable){
+
+        String facilName = r.facilityName;
+        ArrayList days = (ArrayList) r.days;
+
+        Pair<Hashtable, Exception> output = bookingManager.queryAvailability(facilName, days, facilTable);
+
+    }
+}
